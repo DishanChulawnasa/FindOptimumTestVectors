541df3bf807ba1a1393744f90add331bd64045a9
DishanChulawnasa <e17043@eng.pdn.ac.lk>
2023-10-23 19:15:21 +0530

    fixed

--- FindOptimumTestVectors/FindOptimumTestVectors.cpp
+++ FindOptimumTestVectors/FindOptimumTestVectors.cpp
@@ -84,95 +84,111 @@ int main() {
         return 1;
     }
     
-    std::vector<std::string> INTERVectors[500][50];
-    std::vector<std::string> ZVectors[1000];
+    if (inputVectors.size() == 1) {
+        const std::string outputFileName2 = "OptimumTestVectors.txt";
+        std::ofstream outputFile2(outputFileName2);
 
-    performComparisons(inputVectors, INTERVectors[0][0], ZVectors[0]);
-    int emptyINTERVectorIndex = 0;
-    for (int i = 1; i <= 499; ++i) {
-        performComparisons(INTERVectors[i - 1][0], INTERVectors[i][0], ZVectors[0]);
-        if (INTERVectors[i][0].empty()) {
-            emptyINTERVectorIndex = i;
-            break;
+        if (!outputFile2.is_open()) {
+            std::cerr << "Failed to open the file: " << outputFileName2 << std::endl;
+            return 1; // Return an error code
+        }
+
+        for (const std::string& vector : inputVectors) {
+            outputFile2 << vector << "\n";
         }
     }
+    else {
 
-    testVectors.insert(testVectors.end(), INTERVectors[emptyINTERVectorIndex - 1][0].begin(), INTERVectors[emptyINTERVectorIndex - 1][0].end());
+        std::vector<std::string> INTERVectors[500][50];
+        std::vector<std::string> ZVectors[1000];
 
-    std::cout << "TEST VECTOR " << "\n";
-    for (const std::string& vector : testVectors) {
-        std::cout << vector << "\n";
-    }
-
-    std::cout << "Z1:\n";
-    for (const std::string& vector : ZVectors[0]) {
-        std::cout << vector << "\n";
-    }
-
-
-    int emptyZVectorIndex[1000]{};
-    int emptyZVector = 0;
-    for (int iterations = 1; iterations <= 49; ++iterations) {
-
-        performComparisons(ZVectors[iterations-1], INTERVectors[0][iterations], ZVectors[iterations]);
-        //int emptyZVectorIndex = 0;
+        performComparisons(inputVectors, INTERVectors[0][0], ZVectors[0]);
+        int emptyINTERVectorIndex = 0;
         for (int i = 1; i <= 499; ++i) {
-            performComparisons(INTERVectors[i - 1][iterations], INTERVectors[i][iterations], ZVectors[iterations]);
-            if (INTERVectors[i][iterations].empty()) {
-                emptyZVectorIndex[iterations] = i;
+            performComparisons(INTERVectors[i - 1][0], INTERVectors[i][0], ZVectors[0]);
+            if (INTERVectors[i][0].empty()) {
+                emptyINTERVectorIndex = i;
                 break;
             }
         }
-        if (ZVectors[iterations].empty()) {
-            emptyZVector = iterations;
-            break;
-        }
 
-        testVectors.insert(testVectors.end(), INTERVectors[emptyZVectorIndex[iterations] - 1][iterations].begin(), INTERVectors[emptyZVectorIndex[iterations] - 1][iterations].end());
+        testVectors.insert(testVectors.end(), INTERVectors[emptyINTERVectorIndex - 1][0].begin(), INTERVectors[emptyINTERVectorIndex - 1][0].end());
 
-        
-
-        std::cout << "Z2:\n";
-        for (const std::string& vector : ZVectors[iterations]) {
+        std::cout << "TEST VECTOR " << "\n";
+        for (const std::string& vector : testVectors) {
             std::cout << vector << "\n";
         }
 
-    }
-
-    std::cout << "Final Z\n";
-    for (const std::string& vector : ZVectors[emptyZVector - 2]) {
-        std::cout << vector << "\n";
-    }
-
-
-    testVectors.insert(testVectors.end(), ZVectors[emptyZVector - 1].begin(), ZVectors[emptyZVector - 1].end());
-
-    std::set<std::string> uniqueStrings;
-    std::vector<std::string> resulttestvectors;
-
-    for (const std::string& str : testVectors) {
-        if (uniqueStrings.find(str) == uniqueStrings.end()) {
-            // This is a unique string, so we add it to the result vector and the set.
-            uniqueStrings.insert(str);
-            resulttestvectors.push_back(str);
+        std::cout << "Z1:\n";
+        for (const std::string& vector : ZVectors[0]) {
+            std::cout << vector << "\n";
         }
-    }
 
-    const std::string outputFileName = "OptimumTestVectors.txt";
-    std::ofstream outputFile(outputFileName);
 
-    if (!outputFile.is_open()) {
-        std::cerr << "Failed to open the file: " << outputFileName << std::endl;
-        return 1; // Return an error code
-    }
+        int emptyZVectorIndex[1000]{};
+        int emptyZVector = 0;
+        for (int iterations = 1; iterations <= 49; ++iterations) {
 
-    for (const std::string& vector : resulttestvectors) {
-        outputFile << vector << "\n";
-    }
+            performComparisons(ZVectors[iterations - 1], INTERVectors[0][iterations], ZVectors[iterations]);
+            //int emptyZVectorIndex = 0;
+            for (int i = 1; i <= 499; ++i) {
+                performComparisons(INTERVectors[i - 1][iterations], INTERVectors[i][iterations], ZVectors[iterations]);
+                if (INTERVectors[i][iterations].empty()) {
+                    emptyZVectorIndex[iterations] = i;
+                    break;
+                }
+            }
+            if (ZVectors[iterations].empty()) {
+                emptyZVector = iterations;
+                break;
+            }
 
-    std::cout << "TEST VECTORS\n";
-    for (const std::string& vector : resulttestvectors) {
-        std::cout << vector << "\n";
+            testVectors.insert(testVectors.end(), INTERVectors[emptyZVectorIndex[iterations] - 1][iterations].begin(), INTERVectors[emptyZVectorIndex[iterations] - 1][iterations].end());
+
+
+
+            std::cout << "Z2:\n";
+            for (const std::string& vector : ZVectors[iterations]) {
+                std::cout << vector << "\n";
+            }
+
+        }
+
+        std::cout << "Final Z\n";
+        for (const std::string& vector : ZVectors[emptyZVector - 2]) {
+            std::cout << vector << "\n";
+        }
+
+
+        testVectors.insert(testVectors.end(), ZVectors[emptyZVector - 1].begin(), ZVectors[emptyZVector - 1].end());
+
+        std::set<std::string> uniqueStrings;
+        std::vector<std::string> resulttestvectors;
+
+        for (const std::string& str : testVectors) {
+            if (uniqueStrings.find(str) == uniqueStrings.end()) {
+                // This is a unique string, so we add it to the result vector and the set.
+                uniqueStrings.insert(str);
+                resulttestvectors.push_back(str);
+            }
+        }
+
+        const std::string outputFileName = "OptimumTestVectors.txt";
+        std::ofstream outputFile(outputFileName);
+
+        if (!outputFile.is_open()) {
+            std::cerr << "Failed to open the file: " << outputFileName << std::endl;
+            return 1; // Return an error code
+        }
+
+        for (const std::string& vector : resulttestvectors) {
+            outputFile << vector << "\n";
+        }
+
+        std::cout << "TEST VECTORS\n";
+        for (const std::string& vector : resulttestvectors) {
+            std::cout << vector << "\n";
+        }
     }
 
     return 0;
